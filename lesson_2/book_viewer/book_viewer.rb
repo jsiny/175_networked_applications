590d07a9513343646062f877a5e4d9a5bb13bc3a
Juliette Sinibardy <juju.siny@gmail.com>
2019-10-09 16:28:04 +0200

    :sparkles: Add walk-through on book viewer app

--- lesson_2/book_viewer/book_viewer.rb
+++ lesson_2/book_viewer/book_viewer.rb
@@ -2,17 +2,67 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(chapter)
+    chapter.split("\n\n").map.with_index do |line, index|
+      "<p id=paragraph_#{index}>#{line}</p>"
+    end.join
+  end
+
+  def emphasize(text, word)
+    text.gsub(word, "<strong>#{word}</strong>")
+  end
+end
+
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @toc = File.readlines("data/toc.txt")
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @toc = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @toc[number - 1]
+
+  redirect '/' unless (1..@toc.size).cover?(number)
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
 
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+not_found do
+  redirect '/'
+end
+
+def each_chapter
+  @toc.each_with_index do |name, i|
+    number = i + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include? query
+    end
+    results << { number: number, name: name, paragraphs: matches } if matches.any?
+  end
+
+  results
+end
