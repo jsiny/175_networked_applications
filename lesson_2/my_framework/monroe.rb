16fe464706fc33e81ba53b1cebd2502d0ca41dd2
Juliette Sinibardy <juju.siny@gmail.com>
2019-10-09 08:38:23 +0200

    :alembic: Playing around with Rack

---
+++ lesson_2/my_framework/monroe.rb
@@ -0,0 +1,13 @@
+class Monroe
+  def erb(view, local = {})
+    b = binding
+    message = local[:message]
+    template = File.read("views/#{view}.erb")
+    ERB.new(template).result(b)
+  end
+  
+  def response(status, headers, body = '')
+    body = yield if block_given?
+    [status, headers, [body]]
+  end
+end
