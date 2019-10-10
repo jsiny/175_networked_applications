1d168815db77933f75e2579715e5a920bda2c60a
Juliette Sinibardy <juju.siny@gmail.com>
2019-10-10 11:53:01 +0200

    :tada: Start to do list project

---
+++ lesson_5/todo.rb
@@ -0,0 +1,34 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list
+end
+
+# Create a new list
+post "/lists" do
+  session[:lists] << { name: params[:list_name], todos: [] }
+  session[:success] = "The list has been created."
+  redirect "/lists"
+end
