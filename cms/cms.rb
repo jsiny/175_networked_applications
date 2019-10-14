316a72a98f23588a4d79664ada86749367063af1
Juliette Sinibardy <juju.siny@gmail.com>
2019-10-14 08:21:13 +0200

    :white_check_mark: Add first tests to CMS app

--- cms/cms.rb
+++ cms/cms.rb
@@ -2,13 +2,28 @@ require 'sinatra'
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret_cms'
+end
+
+before do
+  @root = File.expand_path("..", __FILE__)
+  @files = Dir.glob(@root + "/data/*").map { |file| File.basename(file) }
+end
+
 get "/" do
-  @files = Dir.glob("data/*").map { |file| File.basename(file) }
   erb :index
 end
 
 get "/:file" do
   @file = params[:file]
-  headers["Content-Type"] = "text/plain"
-  File.readlines("data/#{@file}", "\n\n")
+
+  if @files.include? @file
+    headers["Content-Type"] = "text/plain"
+    File.read("data/#{@file}")
+  else
+    session[:message] = "#{@file} does not exist."
+    redirect "/"
+  end
 end
