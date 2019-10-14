d234a541d80d8f4c1b0d756e44a2a365515915fd
Juliette Sinibardy <juju.siny@gmail.com>
2019-10-14 08:21:13 +0200

    :white_check_mark: Add first tests to CMS app

---
+++ cms/test/cms_test.rb
@@ -0,0 +1,43 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt" 
+  end
+
+  def test_viewing_text_document
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "Africa, the world's wildest continent."
+  end
+
+  def test_document_not_found
+    get "/notafile.txt"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_includes last_response.body, "notafile.txt does not exist"
+
+    get "/"
+    refute_includes last_response.body, "notafile.txt does not exist"
+  end
+end
